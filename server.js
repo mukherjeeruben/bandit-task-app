fbf77eea22feebd42c0cd25b51e63970eff90e48
mukherjeeruben <rubenmukherjee@outlook.com>
2022-05-31 18:54:38 +0100

    app name added

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/bandit-task-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-name>/index.html'));});
+'/dist/bandit-task-app/index.html'));});
 app.listen(process.env.PORT || 8080);
